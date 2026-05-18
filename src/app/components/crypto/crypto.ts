9b930fbad1f2050098c0246125dcf5e36d3d0db1
FA <tommasofalia10@gmail.com>
2026-05-18 10:05:31 +0200

    fatto tutto manca connessione api

--- src/app/components/crypto/crypto.ts
+++ src/app/components/crypto/crypto.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Bankservice } from '../../bankservice';
 
 @Component({
   selector: 'app-crypto',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './crypto.html',
   styleUrl: './crypto.css',
 })
 export class Crypto {
+  euroAmount: number = 0;
+  selectedCrypto: string = 'BTC';
+  result: number | null = null;
 
+  rates: { [key: string]: number } = {
+    'BTC': 0.000016,
+    'ETH': 0.00045,
+    'SOL': 0.0075,
+  };
+
+  constructor(private bankService: Bankservice) {}
+
+  convert() {
+    this.result = this.euroAmount * this.rates[this.selectedCrypto];
+  }
+
+  useCurrentBalance() {
+    this.euroAmount = this.bankService.getConto();
+    this.convert();
+  }
 }
--- src/app/components/fiat/fiat.ts
+++ src/app/components/fiat/fiat.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Bankservice } from '../../bankservice';
 
 @Component({
   selector: 'app-fiat',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './fiat.html',
   styleUrl: './fiat.css',
 })
 export class Fiat {
+  euroAmount: number = 0;
+  selectedCurrency: string = 'USD';
+  result: number | null = null;
 
+  rates: { [key: string]: number } = {
+    'USD': 1.08,
+    'GBP': 0.85,
+    'JPY': 163.50,
+  };
+
+  constructor(private bankService: Bankservice) {}
+
+  convert() {
+    this.result = this.euroAmount * this.rates[this.selectedCurrency];
+  }
+
+  useCurrentBalance() {
+    this.euroAmount = this.bankService.getConto();
+    this.convert();
+  }
 }
